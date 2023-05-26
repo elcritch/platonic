f1b268029dd6af3daefdc8d9e42430324ce62dba
Jaremy Creechley <creechley@gmail.com>
2023-05-26 15:27:35 -0700

    non generics

--- src/platonic/dynamics.nim
+++ src/platonic/dynamics.nim
@@ -11,7 +11,7 @@ type
     mat[int, int] is M.dType
     mvar[int, int] = M.dType
   
-    mvar.init(int)
+    mvar.init(int, int)
 
     # type TransposedType = stripGenericParams(M)[T]
   
@@ -23,7 +23,7 @@ when isMainModule:
   # Example Procs
   # =============
 
-  proc transposed*(m: Matrix): Matrix =
+  proc transposed*[M: Matrix](m: M): M =
     result.init(m.rows, m.cols)
     for r in 0 ..< m.rows:
       for c in 0 ..< m.cols:
@@ -49,8 +49,8 @@ when isMainModule:
     result.m = m
     result.n = n
 
-  proc init*[T](mat: var MatrixImplF64, m, n: int) =
-    mat.data = newSeq[T](m*n)
+  proc init*(mat: var MatrixImplF64, m, n: int) =
+    mat.data = newSeq[float64](m*n)
     mat.m = m
     mat.n = n
 
@@ -59,10 +59,10 @@ when isMainModule:
   proc cols*(mat: MatrixImplF64): int = mat.n
   proc size*(mat: MatrixImplF64): int = mat.m*mat.n
 
-  proc `[]`*(mat: MatrixImplF64; m, n: int): MatrixImplF64.dType() =
+  proc `[]`*(mat: MatrixImplF64; m, n: int): float64 =
     mat.data[m * mat.rows() + n]
 
-  proc `[]=`*(M: var MatrixImplF64; m, n: int; v: MatrixImplF64.dType) =
+  proc `[]=`*(M: var MatrixImplF64; m, n: int; v: float64) =
     M.data[m * M.rows + n] = v
 
   var
@@ -70,6 +70,6 @@ when isMainModule:
     projectionMatrix: MatrixImplF64 = initMatrixImplF64(3, 3)
 
   let m1: MatrixImplF64 = m.transposed()
-  echo m.transposed
-  echo m.transposed.determinant
+  echo "transposed: ", m.transposed
+  echo "transposed:det: ", m.transposed.determinant
   setPerspectiveProjection projectionMatrix
